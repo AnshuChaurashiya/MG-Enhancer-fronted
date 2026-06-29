56e9515b72737148a3f7436df61f494ae47c8bdd
Anshu Chaurasiya <154973282+AnshuChaurashiya@users.noreply.github.com>
2026-06-29 02:05:11 -0700

    solve the user auto errs

--- Middleware/UserMiddleware.js
+++ Middleware/UserMiddleware.js
@@ -9,7 +9,7 @@ module.exports.autUser = async (req, res, next) => {
     
     }
     
-    
+     
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
         const user = await UserModel.findById(decoded.id)
