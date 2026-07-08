9fe12f2245d52d5b2de57c3facc3ac54f5b767c6
Anshu Chaurasiya <154973282+AnshuChaurashiya@users.noreply.github.com>
2026-07-07 23:27:31 -0700

    Refactor registerUser function in UserController
    Refactor user registration function declaration.

--- controller/UserController.js
+++ controller/UserController.js
@@ -3,8 +3,7 @@ const UserModel = require("../Models/UserModels");
 const UserService = require("../service/User.Service");
 const {validationResult} = require("express-validator");
 
- // this is the user related code and user auth and dashbord page
-module.exports.registerUser = async (req, res, next) => {
+ module.exports.registerUser = async (req, res, next) => {
     try {
 
         const errors = validationResult(req);
