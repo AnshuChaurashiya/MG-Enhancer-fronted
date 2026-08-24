32fd7ab197b35825f84958221e905779bc31de8a
Anshu Chaurasiya <154973282+AnshuChaurashiya@users.noreply.github.com>
2026-08-24 03:17:03 -0700

    Refactor createUser function for cleaner code

--- service/User.Service.js
+++ service/User.Service.js
@@ -1,26 +1,18 @@
 const UserModel = require("../Models/UserModels");
 // this is the user service page 
-
 module.exports.createUser = async (userData) => {
     const {name, email, password} = userData;
-
     try {
-
         if(!userData.name || !userData.email || !userData.password) {
             throw new Error("All fields are required")
         }
-
         const user = await UserModel.create({
             name: userData.name,
             email: userData.email,
             password: userData.password,
         })
-        return user
-        
-        
-        
+        return user    
     } catch (error) {
         throw new Error(error, "There was an error creating the user")
-
     }
 }
