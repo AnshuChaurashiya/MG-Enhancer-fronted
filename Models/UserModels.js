518a750633805b5262a97c86fb30b795df744b0a
Anshu Chaurasiya <154973282+AnshuChaurashiya@users.noreply.github.com>
2026-08-24 23:57:46 -0700

    Add comparePassword method to user schema

--- Models/UserModels.js
+++ Models/UserModels.js
@@ -29,6 +29,7 @@ userSchema.methods.generateAuthToken = function () {
     return token;
 }
 
+
 userSchema.methods.comparePassword = async function (password) {
     const isMatch = await bcrypt.compare(password, this.password);
     return isMatch;
